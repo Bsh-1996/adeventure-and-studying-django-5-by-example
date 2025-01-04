e1023fa47eb65f784cde51c7be6ade52f8ed83c3
behrouzsh <behrouzsharifi7@gmail.com>
2025-01-04 19:02:05 +0330

    Creating SEO-friendly URLs for posts

--- A/blog/models.py
+++ A/blog/models.py
@@ -17,7 +17,10 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'PUBLISHED'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=2500)
+    slug = models.SlugField(
+        max_length=2500,
+        unique_for_date= 'publish'
+        )
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
