6fe6a6f9f5fb9110e91967bec2e83dcddc6a60f0
behrouzsh <behrouzsharifi7@gmail.com>
2025-01-05 13:50:41 +0330

    Creating forms with Django

--- A/blog/views.py
+++ A/blog/views.py
@@ -5,6 +5,19 @@ from django.views.generic import ListView
 
 from .models import Post
 
+#post_list class based view
+class PostListView(ListView):
+    """
+    Alternative post List view
+    """
+
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+#post list based on function view. it has the same functionality
+"""
 def post_list(request):
     post_list = Post.published.all()
 
@@ -21,7 +34,7 @@ def post_list(request):
         posts = paginator.page(paginator.num_pages)
 
     return render(request, 'blog/post/list.html', {'posts': posts})
-
+"""
 
 
 
@@ -29,19 +42,11 @@ def post_detail(request, year, month, day, post):
     post = get_object_or_404(
         Post,
         status = Post.Status.PUBLISHED,
+        slug = post,
         publish__year = year,
         publish__month = month,
         publish__day = day
         )
     return render(request, 'blog/post/detail.html', {'post': post})
 
-class PostListView(ListView):
-    """
-    Alternative post List view
-    """
 
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
-    
